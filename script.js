b99824324e407e27e24319b85a9e6ec80dc0f8c6
Jaya Winata <jayawinata100@gmail.com>
2024-01-15 15:17:36 +0700

    script done

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const list = document.getElementById('list');
 
-function addList(selected = false) {
+function addList(value, selected = false) {
     const element = document.createElement('li');
-    const content = document.getElementById('input').value;
-    if (content) {
-        element.innerHTML = content;
+    if (value) {
+        element.innerHTML = value;
         document.getElementById('list').appendChild(element);
         document.getElementById('input').value = '';
         element.addEventListener('click', () => {
@@ -13,10 +12,12 @@ function addList(selected = false) {
     }
 }
 
-document.getElementById('addButton').addEventListener('click',addList);
+document.getElementById('addButton').addEventListener('click', () => {
+    addList(document.getElementById('input').value);
+});
 document.getElementById('input').addEventListener('keypress',(event) => {
     if (event.key === 'Enter') {
-        addList();
+        addList(document.getElementById('input').value);
     }
 })
 
@@ -37,3 +38,29 @@ function emptyList() {
 }
 
 document.getElementById('elButton').addEventListener('click',emptyList);
+
+function saveList() {
+    const todos = [];
+    for (let i = 0; i < list.children.length; i++) {
+        let todo = list.children.item(i);
+        let todoObj = {
+            'task': todo.innerHTML,
+            'completed': todo.classList.contains('selected')
+        };
+        todos.push(todoObj);
+    }
+    localStorage.setItem('todos',JSON.stringify(todos));
+}
+
+document.getElementById('slButton').addEventListener('click',saveList)
+
+function loadList() {
+    if (localStorage.getItem('todos')) {
+        const todos = JSON.parse(localStorage.getItem('todos'));
+        for (let i = 0; i < todos.length; i++) {
+            addList(todos[i].task,todos[i].completed);
+        }
+    }
+}
+
+loadList();
